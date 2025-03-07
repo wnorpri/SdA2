6e409e094dc8f68175531d5228dcaa7a5b553148
wnorpri <wnorpri498@g.educaand.es>
2025-03-07 23:22:21 +0100

    Add files via upload

---
+++ actividad 4/js/script.js
@@ -0,0 +1,7 @@
+function Calcular() {
+    let a;
+    let b;
+    a = parseInt(prompt(" Introduce el primer número "),10);
+    b = parseInt(prompt(" Introduce el segundo número "),10);
+    alert(" El primer numero es: "+ a + "\n El segundo número es: " + b  + " \n La suma es:" + ( a + b ) + "\n La resta es: " +( a - b ) + "\n El producto es:" + (a * b) + "\n La división es:" + (a / b) + "\n El resto es: " + (a % b))
+}
