c482ac7a07f2cd7a2d989de31789e6f20bdc3a1a
wnorpri <wnorpri498@g.educaand.es>
2025-03-07 16:53:06 +0100

    Delete actividad 3 directory

--- actividad 3/js/script.js
+++
@@ -1,7 +0,0 @@
-function pedirDatos() {
-    let nombre;
-    let edad;
-    nombre =prompt(" ¿Cómo te llamas? ");
-    edad= prompt(" ¿Cuántos años tienes? ");
-    alert(" Hola " + nombre + " , veo que tienes " + edad + " años. ");
-}
