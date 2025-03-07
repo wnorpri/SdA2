3419b16e8c59400fa17fbff46fcdac9a72ff88ed
wnorpri <wnorpri498@g.educaand.es>
2025-03-07 17:03:04 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function pedirDatos() {
+    let nombre;
+    let edad;
+    nombre =prompt(" ¿Cómo te llamas? ");
+    edad= prompt(" ¿Cuántos años tienes? ");
+    alert(" Hola " + nombre + " , veo que tienes " + edad + " años. ");
+}
